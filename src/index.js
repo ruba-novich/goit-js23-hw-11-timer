3fe7ecea2d1bf0918b4eca5c3cf7485a5f6c3a4b
ruba-novich <rubanovichartem@gmail.com>
2020-12-02 21:13:21 +0200

    12
    рефы, и функцию updateClockface перенес в класс.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import './styles.css';
 
-const refs = {
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  secs: document.querySelector('span[data-value="secs"]'),
-} 
+ 
+
 
 class CountdownTimer {
-  constructor({ selector, targetDate, onTick }) {
+  constructor({ selector, targetDate,}) {
     this.selector = selector;
     this.targetDate = targetDate;
-    this.onTick = onTick;
     this.intervalId = null;
     
+    this.refs = {
+      $days: document.querySelector(`${selector} [data-value="days"]`),
+      $hours: document.querySelector(`${selector} [data-value="hours"]`),
+      $mins: document.querySelector(`${selector} [data-value="mins"]`),
+      $secs: document.querySelector(`${selector} [data-value="secs"]`),   
+  }
+
     this.start();
       
   }
@@ -22,9 +24,8 @@ class CountdownTimer {
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = this.targetDate - currentTime;
-      // this.renderTime(getTimeComponents(deltaTime));
       const time = this.getTimeComponents(deltaTime);
-      this.onTick(time);
+      this.updateClockface(time);
     },1000);
   }
 
@@ -34,24 +35,27 @@ class CountdownTimer {
     const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
     return { days, hours, mins, secs };
   }
   
   pad(value) {
   return String(value).padStart(2, '0');
   }
+
+  updateClockface({ days, hours, mins, secs }) {
+    this.refs.$days.innerHTML = days;
+    this.refs.$hours.innerHTML = hours;
+    this.refs.$mins.innerHTML = mins;
+    this.refs.$secs.innerHTML = secs;
+  }
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1', 
   targetDate: new Date('Jan 01, 2021'),
-  onTick: updateClockface,
+  
 });
 
-function updateClockface({ days, hours, mins, secs }) {
-  refs.days.textContent = `${days}`;
-  refs.hours.textContent = `${hours}`;
-  refs.mins.textContent = `${mins}`;
-  refs.secs.textContent = `${secs}`;
-}
+
 
